a4179d83f1b7b61711c21b0b3b85464bbafa5cf6
SahilPanigrahi <119133054+SahilPanigrahi@users.noreply.github.com>
2024-03-23 10:42:52 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,9 +7,7 @@ const cors = require('cors');
 
 dotenv.config();
 
-
-const url = 'mongodb://localhost:27017';
-const client = new MongoClient(url);
+const client = new MongoClient(process.env.MONGODB_URI);
 
 const dbName = "Personal-File";
 const app = express();
@@ -46,53 +44,3 @@ app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
 
-
-
-
-// Connect to MongoDB
-// mongoose.connect(process.env.MONGODB_URI, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true
-//   }).then(() => {
-//     console.log('Connected to MongoDB');
-//   }).catch(err => console.error('Error connecting to MongoDB:', err));
-  
-//   // Define MongoDB schema and model
-//   const FormEntry = mongoose.model('FormEntry', {
-//     user_name: String,
-//     user_email: String,
-//     user_project: String
-//   });
-
-
-
-
-
-// Handle form submission
-// app.post('/submit-form', async (req, res) => {
-//   try {
-//     const { user_name, user_email, user_project } = req.body;
-//     const newFormEntry = new FormEntry({
-//       user_name,
-//       user_email,
-//       user_project
-//     });
-//     await newFormEntry.save();
-//     res.status(201).send('Form data saved successfully');
-//   } catch (error) {
-//     console.error('Error saving form data:', error);
-//     res.status(500).send('Internal Server Error');
-//   }
-// });
-
-
-
-
-
-
-
-// // Middleware to parse JSON
-// app.use(express.json());
-
-// // Serve static files (optional, if you have static files to serve)
-// app.use(express.static('public'));
